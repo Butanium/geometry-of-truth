33f45f0a0bbc299761226b14d63e906cb22303a6
Clément Dumas <clement.dumas@ens-paris-saclay.fr>
2024-01-25 23:55:54 +0000

    Added command line arguments

--- xor_en_sp.py
+++ xor_en_sp.py
@@ -1,19 +1,30 @@
 from probes import LRProbe
 from utils import DataManager
 import torch as th
+from tqdm import tqdm
+import time
+from transformers import AutoConfig
+from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument("--model", type=str, default="EleutherAI/pythia-410m-deduped")
+parser.add_argument("--checkpoint-start", type=int, default=0)
+args = parser.parse_args()
 label_names = [
     "has_alice",
     "has_en",
     "has_alice xor has_en",
 ]
-DEVICE = "auto"
+DEVICE = "cuda"
 if DEVICE == "auto":
     DEVICE = "cuda" if th.cuda.is_available() else "cpu"
-all_checkpoints = [0] + [2**i for i in range(10)] + [1000 * 2**i for i in range(8)] + [143_000]
-model = "EleutherAI/pythia-70m-deduped"
+all_checkpoints = (
+    [0] + [2**i for i in range(10)] + [1000 * 2**i for i in range(8)] + [143_000]
+)
+model = args.model
+cfg = AutoConfig.from_pretrained(model)
 all_accs = {}
-for step in all_checkpoints:
+for step in tqdm(all_checkpoints[args.checkpoint_start:]):
     revision = f"step{step}"
     accs = {}
     for label_name in label_names:
@@ -21,7 +32,7 @@ for step in all_checkpoints:
         dm.add_dataset(
             "sentences_alice",
             model,
-            4,
+            int(0.65 * cfg.num_hidden_layers),
             label=label_name,
             center=False,
             split=0.8,
@@ -39,6 +50,11 @@ import plotly.express as px
 
 all_accs_f = {k: {k2: v2.item() for k2, v2 in v.items()} for k, v in all_accs.items()}
 
-fig = px.bar(all_accs_f, barmode='group')
-fig.update_layout(xaxis_title="Feature", yaxis_title="Accuracy", legend_title="Revision")
-fig.write_html("plot.html")
+fig = px.bar(all_accs_f, barmode="group")
+fig.update_layout(
+    xaxis_title="Feature",
+    yaxis_title="Accuracy",
+    legend_title="Revision",
+    title=f"{model}, layer {int(0.65 * cfg.num_hidden_layers)}",
+)
+fig.write_html("plot" + str(time.time()) + ".html")
