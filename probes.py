1876f749d9bd224c42ebb5f5101c6901e22d8d68
Clément Dumas <clement.dumas@ens-paris-saclay.fr>
2024-01-25 12:50:06 +0000

    added sklearn and elk

--- probes.py
+++ probes.py
@@ -1,10 +1,12 @@
-import torch as t
+import torch as th
+import torch.nn as nn
+from sklearn.linear_model import LogisticRegression
 
 
-class LRProbe(t.nn.Module):
+class LRProbe(nn.Module):
     def __init__(self, d_in, bias=False):
         super().__init__()
-        self.net = t.nn.Sequential(t.nn.Linear(d_in, 1, bias=bias), t.nn.Sigmoid())
+        self.net = nn.Sequential(nn.Linear(d_in, 1, bias=bias), nn.Sigmoid())
 
     def forward(self, x, iid=None):
         return self.net(x).squeeze(-1)
@@ -13,42 +15,87 @@ class LRProbe(t.nn.Module):
         return self(x).round()
 
     def from_data(
-        acts, labels, bias=False, lr=0.001, weight_decay=0.1, epochs=1000, device="cpu"
+        acts,
+        labels,
+        bias=False,
+        lr=0.001,
+        weight_decay=0.1,
+        epochs=10_000,
+        device="cpu",
+        algo="sklearn",
     ):
-        acts, labels = acts.to(device), labels.to(device)
         probe = LRProbe(acts.shape[-1], bias=bias).to(device)
+        probe.fit(
+            acts,
+            labels,
+            lr=lr,
+            weight_decay=weight_decay,
+            epochs=epochs,
+            device=device,
+            algo=algo,
+        )
 
-        opt = t.optim.AdamW(probe.parameters(), lr=lr, weight_decay=weight_decay)
-        for _ in range(epochs):
-            opt.zero_grad()
-            loss = t.nn.BCELoss()(probe(acts), labels)
-            loss.backward()
-            opt.step()
-
-        return probe
+    def fit(
+        self,
+        acts,
+        labels,
+        lr=0.001,
+        weight_decay=0.1,
+        epochs=10_000,
+        device="cpu",
+        algo="sklearn",
+    ):
+        if algo == "sklearn":
+            classifier = LogisticRegression(
+                class_weight="balanced", solver="newton-cholesky"
+            )
+            classifier.fit(acts.cpu().numpy(), labels.cpu().numpy())
+            self.net[0].weight.data.copy_(th.tensor(classifier.coef_, device=device))
+            self.net[0].bias.data.copy_(th.tensor(classifier.intercept_, device=device))
+        elif algo == "sgd":
+            acts, labels = acts.to(device), labels.to(device)
+            opt = th.optim.AdamW(self.parameters(), lr=lr, weight_decay=weight_decay)
+            for _ in range(epochs):
+                opt.zero_grad()
+                loss = th.nn.BCELoss()(self(acts), labels)
+                loss.backward()
+                opt.step()
+        elif algo == "elk":
+            try:
+                from elk.training.classifier import Classifier
+            except ImportError:
+                raise ImportError("Please install elk using pip install eleuther-elk")
+            classifier = Classifier(acts.shape[-1], num_classes=1, device=device)
+            classifier.fit(acts, labels, l2_penalty=weight_decay, max_iter=epochs)
+            self.net[0].weight.data.copy_(classifier.linear.weight.data)
+            self.net[0].bias.data.copy_(classifier.linear.bias.data)
+        else:
+            raise ValueError(
+                f"Unknown algo {algo}. Must be one of 'sklearn', 'sgd', or 'elk'"
+            )
 
     @property
     def direction(self):
         return self.net[0].weight.data[0]
 
 
-class MMProbe(t.nn.Module):
+class MMProbe(nn.Module):
     def __init__(self, direction, covariance=None, inv=None, atol=1e-3):
         super().__init__()
-        self.direction = t.nn.Parameter(direction, requires_grad=False)
+        self.direction = nn.Parameter(direction, requires_grad=False)
         if inv is None:
-            self.inv = t.nn.Parameter(
-                t.linalg.pinv(covariance, hermitian=True, atol=atol),
+            self.inv = nn.Parameter(
+                th.linalg.pinv(covariance, hermitian=True, atol=atol),
                 requires_grad=False,
             )
         else:
-            self.inv = t.nn.Parameter(inv, requires_grad=False)
+            self.inv = nn.Parameter(inv, requires_grad=False)
 
     def forward(self, x, iid=False):
         if iid:
-            return t.nn.Sigmoid()(x @ self.inv @ self.direction)
+            return nn.Sigmoid()(x @ self.inv @ self.direction)
         else:
-            return t.nn.Sigmoid()(x @ self.direction)
+            return nn.Sigmoid()(x @ self.direction)
 
     def pred(self, x, iid=False):
         return self(x, iid=iid).round()
@@ -59,7 +106,7 @@ class MMProbe(t.nn.Module):
         pos_mean, neg_mean = pos_acts.mean(0), neg_acts.mean(0)
         direction = pos_mean - neg_mean
 
-        centered_data = t.cat([pos_acts - pos_mean, neg_acts - neg_mean], 0)
+        centered_data = th.cat([pos_acts - pos_mean, neg_acts - neg_mean], 0)
         covariance = centered_data.t() @ centered_data / acts.shape[0]
 
         probe = MMProbe(direction, covariance=covariance).to(device)
@@ -71,14 +118,14 @@ def ccs_loss(probe, acts, neg_acts):
     p_pos = probe(acts)
     p_neg = probe(neg_acts)
     consistency_losses = (p_pos - (1 - p_neg)) ** 2
-    confidence_losses = t.min(t.stack((p_pos, p_neg), dim=-1), dim=-1).values ** 2
-    return t.mean(consistency_losses + confidence_losses)
+    confidence_losses = th.min(th.stack((p_pos, p_neg), dim=-1), dim=-1).values ** 2
+    return th.mean(consistency_losses + confidence_losses)
 
 
-class CCSProbe(t.nn.Module):
+class CCSProbe(nn.Module):
     def __init__(self, d_in):
         super().__init__()
-        self.net = t.nn.Sequential(t.nn.Linear(d_in, 1, bias=False), t.nn.Sigmoid())
+        self.net = nn.Sequential(nn.Linear(d_in, 1, bias=False), nn.Sigmoid())
 
     def forward(self, x, iid=None):
         return self.net(x).squeeze(-1)
@@ -98,7 +145,7 @@ class CCSProbe(t.nn.Module):
         acts, neg_acts = acts.to(device), neg_acts.to(device)
         probe = CCSProbe(acts.shape[-1]).to(device)
 
-        opt = t.optim.AdamW(probe.parameters(), lr=lr, weight_decay=weight_decay)
+        opt = th.optim.AdamW(probe.parameters(), lr=lr, weight_decay=weight_decay)
         for _ in range(epochs):
             opt.zero_grad()
             loss = ccs_loss(probe, acts, neg_acts)
