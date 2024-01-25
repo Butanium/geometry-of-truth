a9e4736385bbca5de000624f296da75c91d7c365
Clément Dumas <clement.dumas@ens-paris-saclay.fr>
2024-01-25 12:37:15 +0000

    Added seend random_init and shuffle
    WIP on accross time
    
    Co-authored-by: Grégoire DHIMOÏLA <Parabrele@users.noreply.github.com>

--- xor_across_time.py
+++ xor_across_time.py
@@ -31,10 +31,13 @@ def xor_results(
     model,
     device,
     layers=None,
+    seed=None,
     checkpoints=None,
     compute_acts=True,
     batch_size=1,
     chunk_size=25,
+    random_init=False,
+    shuffle=False,
 ):
     """
     Compute the accuracy of a logistic regression probe on some XOR features
@@ -43,29 +46,46 @@ def xor_results(
     if device == "auto":
         device = "cuda" if th.cuda.is_available() else "cpu"
         print(f"Using device {device}")
-    if checkpoints is None:
+    if checkpoints == [-1]:
         checkpoints = all_checkpoints
+    if checkpoints is None or checkpoints == []:
+        checkpoints = ["trained"]
+    if random_init:
+        checkpoints.append("random init")
+    if shuffle:
+        checkpoints.append("shuffle")
     if layers is None:
         config = AutoConfig.from_pretrained(model)
         layers = list(range(config.num_hidden_layers + 1))
     if compute_acts:
         for checkpoint in checkpoints:
+            if isinstance(checkpoint, int):
+                revision = f"step{checkpoint}"
+                checkpoint = f"Step {checkpoint}"
+            else:
+                revision = None
             print(f"Generating activations for checkpoint {checkpoint}")
             generate_acts(
                 model,
                 layers,
                 ["cities_alice", "neg_cities_alice"],
                 device=device,
-                revision=f"step{checkpoint}",
+                revision=revision,
                 batch_size=batch_size,
                 chunk_size=chunk_size,
+                random_init=checkpoint == "random init",
+                shuffle=checkpoint == "shuffle",
             )
     layer_accs = {}
     for layer in layers:
         checkpoint_accs = {}
-        for i in checkpoints:
-            revision = f"step{i}"
-            print(f"Layer {layer}, checkpoint {revision}")
+        for checkpoint in checkpoints:
+            if isinstance(checkpoint, int):
+                revision = f"step{checkpoint}"
+                checkpoint = f"Step {checkpoint}"
+            else:
+                revision = None
+            print(f"Layer {layer}, {checkpoint}")
             accs = {}
             for label_name in label_names:
                 dm = DataManager()
@@ -74,18 +94,21 @@ def xor_results(
                         dataset,
                         model,
                         layer,
+                        seed=seed,
                         label=label_name,
                         center=False,
                         split=0.8,
                         device=device,
                         revision=revision,
+                        shuffle=checkpoint == "shuffle",
+                        random_init=checkpoint == "random init",
                     )
                 acts, labels = dm.get("train")
                 probe = LRProbe.from_data(acts, labels, bias=True, device=device)
                 acts, labels = dm.get("val")
                 acc = (probe(acts).round() == labels).float().mean()
                 accs[label_name] = acc.item()
-            checkpoint_accs[revision] = accs
+            checkpoint_accs[checkpoint] = accs
         layer_accs[layer] = checkpoint_accs
     return layer_accs
 
@@ -105,29 +128,48 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--checkpoints",
-        nargs="+",
+        nargs="*",
         type=int,
-        help="Checkpoints to probe. Default: All checkpoints on a log scale",
-        default=None,
+        help="Checkpoints to probe. Default: -1 (all checkpoints on a log scale)."
+        "If None are given, only the trained model is probed.",
+        default=[-1],
     )
     parser.add_argument(
-        "--disable-acts-computation",
+        "--no-acts",
         action="store_true",
         default=False,
-        help="Set flag to disable computation of activations. If it is not given,"
+        help="Set flag to disable computation of activations (if you already computed them). If it is not given,"
         "activations will be computed for all checkpoints before probing.",
     )
     parser.add_argument(
         "--batch-size",
         type=int,
-        default=1,
+        default=32,
         help="Batch size for generating activations",
     )
     parser.add_argument(
         "--chunk-size",
         type=int,
-        default=25,
+        default=64,
         help="Number of activations to save per file",
+    )
+    parser.add_argument(
+        "--random-init",
+        action="store_true",
+        default=False,
+        help="Set flag to add a randomly initialized model in addition to the checkpoints",
+    )
+    parser.add_argument(
+        "--shuffle",
+        action="store_true",
+        default=False,
+        help="Set flag to add a model with the trained weights shuffled in addition to the checkpoints",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=None,
+        help="Seed for experiments",
     )
     args = parser.parse_args()
 
@@ -137,9 +179,12 @@ if __name__ == "__main__":
         args.device,
         layers=args.layers,
         checkpoints=args.checkpoints,
-        compute_acts=not args.disable_acts_computation,
+        compute_acts=not args.no_acts,
         batch_size=args.batch_size,
         chunk_size=args.chunk_size,
+        random_init=args.random_init,
+        shuffle=args.shuffle,
+        seed=args.seed,
     )
 
     # Save results
@@ -158,10 +203,11 @@ if __name__ == "__main__":
     time_id = int(time.time())
     df.to_csv(path / f"results_{time_id}.csv")
 
-    # Plot
-    fig = make_subplots(rows=1, cols=1, specs=[[{"type": "bar"}]])
     layers = list(all_accs.keys())
     checkpoint_nbs = list(all_accs[layers[0]].keys())
+    fig = make_subplots(rows=1, cols=1, specs=[[{"type": "bar"}]])
+    fig.update_layout(title=f"Accuracy across depth for {args.model}")
+
     for checkpoint_nb in checkpoint_nbs:
         for layer in layers:
             values = [all_accs[layer][checkpoint_nb][key] for key in label_names]
@@ -201,6 +247,52 @@ if __name__ == "__main__":
     )
     for data in fig.data:
         data.update(visible=f"Layer {layers[0]}" in data.name)
-        data.name = data.name.split(", ")[1]
     fig.update_yaxes(range=[0, 1])
-    fig.write_html(path / f"interactive_{time_id}.html")
+    fig.write_html(path / f"interactive_across_depth{time_id}.html")
+
+    # Plot interactive results across time
+    fig = make_subplots(rows=1, cols=1, specs=[[{"type": "bar"}]])
+
+    for checkpoint_nb in checkpoint_nbs:
+        for layer in layers:
+            values = [all_accs[layer][checkpoint_nb][key] for key in label_names]
+            fig.add_trace(
+                go.Bar(
+                    x=label_names,
+                    y=values,
+                    name=f"Layer {layer}, {checkpoint_nb}",
+                )
+            )
+    # Add slider
+    fig.update_layout(
+        barmode="group",
+        sliders=[
+            {
+                "active": 0,
+                "yanchor": "top",
+                "xanchor": "left",
+                "currentvalue": {
+                    "font": {"size": 16},
+                    "visible": True,
+                    "prefix": "Checkpoint: ",
+                    "suffix": "",
+                },
+                "pad": {"b": 10, "t": 50},
+                "steps": [
+                    {
+                        "label": str(checkpoint_nb),
+                        "method": "update",
+                        "args": [
+                            {"visible": [checkpoint_nb == c for c in checkpoint_nbs]}
+                        ],
+                    }
+                    for checkpoint_nb in checkpoint_nbs
+                ],
+            }
+        ],
+    )
+    for data in fig.data:
+        data.update(visible=f"Step {checkpoint_nbs[0]}" in data.name)
+    fig.update_yaxes(range=[0, 1])
+    # TODO: fix this before saving
+    # fig.write_html(path / f"interactive_across_time{time_id}.html")
